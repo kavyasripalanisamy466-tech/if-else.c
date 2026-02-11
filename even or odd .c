dd438cb84e0bfdc0ed47c3421bcc6ce24addc80e
kavyasripalanisamy466-tech <kavyasripalanisamy466@gmail.com>
2026-02-11 15:04:06 +0530

    Add files via upload

---
+++ even or odd .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a=7;
+    scanf("%d",&a);
+    if(a%2==0){
+        printf("even number");
+    }
+    else{
+        printf("odd number");
+        return 0;
+    }
+}
