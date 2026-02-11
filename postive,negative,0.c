44c0038a021b5b13cc9c13f8e2b103beaf2af25b
kavyasripalanisamy466-tech <kavyasripalanisamy466@gmail.com>
2026-02-11 14:57:52 +0530

    Add files via upload

---
+++ postive,negative,0.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a=5;
+    scanf("%d",&a);
+    if(a>0)
+    printf("positive number");
+    else if(a<0)
+    
+    printf("negative number");
+    else
+    printf("zero");
+    
+}
