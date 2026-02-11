7bc485b7d2ead8d5b2d6228f42f4470299645637
kavyasripalanisamy466-tech <kavyasripalanisamy466@gmail.com>
2026-02-11 15:49:43 +0530

    Add files via upload

---
+++ greater 2 numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a=10;
+    int b=20;
+    scanf("%d %d",&a,&b);
+    if(a<b){
+        printf("20 is greater");
+    }
+    else
+    printf("20 is not greater");
+    return 0;
+}
