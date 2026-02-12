34ca67e927f6fba29cd55b7b6cc2d509837ee1cc
kavyasripalanisamy466-tech <kavyasripalanisamy466@gmail.com>
2026-02-12 09:59:55 +0530

    Add files via upload

---
+++ alphabet,digits or special characters.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char a;
+    scanf("%c",&a);
+    if((a>65&&a<=90)||(a>=97&&a<=122)){
+        printf("Alphabet");
+    }
+    else if(a>=48 && a<=57){
+        printf("Digit");
+    }
+    else
+    printf("Special character");
+    return 0;
+}
