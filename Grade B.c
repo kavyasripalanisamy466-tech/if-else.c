a68ef8b52e48dae0e3ca7c0b7f177d9ffd8b7957
kavyasripalanisamy466-tech <kavyasripalanisamy466@gmail.com>
2026-02-12 11:14:41 +0530

    Add files via upload

---
+++ Grade B.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if((a>=75)&&(a<90)){
+        printf("Grade B");
+    }
+    else
+    printf("not Grade B");
+    return 0;
+}
