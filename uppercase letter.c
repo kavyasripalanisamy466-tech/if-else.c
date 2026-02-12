2c356baef0f2a8271105c97a109b9faee17172a7
kavyasripalanisamy466-tech <kavyasripalanisamy466@gmail.com>
2026-02-12 11:41:25 +0530

    Add files via upload

---
+++ uppercase letter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char a;
+    scanf("%c",&a);
+    if(a>=65&&a<=90){
+        printf("Uppercase letter");
+    }
+    else
+    printf("not upperecase letter");
+    return 0;
+}
